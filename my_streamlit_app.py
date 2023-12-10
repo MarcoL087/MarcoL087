142355f7e105f4115b8bbf9f43711b52bbe952b0
MarcoL087 <148775781+MarcoL087@users.noreply.github.com>
2023-12-10 21:26:29 +0100

    Update my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -1,7 +1,20 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
 import streamlit as st
 
-st.title('Hello Wilders, welcome to my application!')
+st.title('Hello Wilders, welcome to the best application of the year!')
 
-name = st.text_input("Please give me your name :")
-name_length = len(name)
-st.write("Your name has ",name_length,"characters")
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
+df = pd.read_csv(link)
+
+df
+
+df_correlation = sns.heatmap(df.corr(), 
+								center=0,
+								cmap = sns.color_palette("vlag", as_cmap=True)
+								)
+
+st.pyplot(viz_correlation.figure)

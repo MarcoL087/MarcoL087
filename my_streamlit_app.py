9db2338045352471cfb2ed3abb7a77c3bb659204
MarcoL087 <148775781+MarcoL087@users.noreply.github.com>
2023-12-10 21:03:02 +0100

    Update my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
 st.title('Hello Wilders, welcome to my application!')
-streamlit run my_streamlit_app.py
+
+name = st.text_input("Please give me your name :")
+name_length = len(name)
+st.write("Your name has ",name_length,"characters")
